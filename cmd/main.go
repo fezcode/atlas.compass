98aae3ef1dab9058ba781cab3e29b883d9968c53
fezcode <samil.bulbul@gmail.com>
2026-02-20 23:35:10 +0300

    feat: add version flag support

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,14 @@ import (
 	"github.com/fezcode/atlas.compass/internal/tui"
 )
 
+var Version = "dev"
+
 func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "-v" || os.Args[1] == "--version") {
+		fmt.Printf("atlas.compass v%s\n", Version)
+		return
+	}
+
 	p := tea.NewProgram(tui.NewMainModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Error running atlas.compass: %v\n", err)
